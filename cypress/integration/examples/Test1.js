f01e85a29922d2ddead1c6b110f69497bddba10b
Surbhi Patel <surbhip2112@gmail.com>
2024-03-20 18:14:41 -0400

    Cypress Automation

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,7 @@
+//cypress - SPEC file
+describe("My First Test Suite", () => {
+  it("My First Test Case!", () => {
+    // visit method helps us to visit any website
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+  });
+});
